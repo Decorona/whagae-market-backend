1cdf4a5b0701798786e955d5c56de113087667e2
sniim <snpo_@naver.com>
2020-07-17 21:12:12 +0900

    마켓

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 // Swagger JS Docs
-const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerJSDoc = require('swagger-jsdoc');
 
 // Router
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var marketsRouter = require('./routes/markets');
 
-const db = require("./models");
+const db = require('./models');
 db.sequelize.sync({
   force: false,
 });
 var app = express();
 
 // Swagger With Express
-const options = require("./swagger-jsdoc-options");
+const options = require('./swagger-jsdoc-options');
 
-const swaggerUi = require("swagger-ui-express");
+const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = swaggerJSDoc(options);
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Swagger UI Mount
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/markets', marketsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -48,11 +50,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
---
+++ routes/markets.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+const models = require('../models');
+
+/* GET users listing. */
+/**
+ * @swagger
+ * /markets:
+ *   get:
+ *     consumes:
+ *       — application/json
+ *     parameters:
+ *       — name: get
+ *       in: get
+ *       schema:
+ *         type: object
+ *         properties:
+ *     responses:
+ *       200:
+ *         description: Receive back MarketModel
+ */
+router.get('/', function (req, res, next) {
+  models.Market.findAll()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await models.Market.findOne({
+      where: {id: req.params.id},
+    });
+
+    res.json(user);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+module.exports = router;
